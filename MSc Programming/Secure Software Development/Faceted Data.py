3d4fa5eb3e9c3ac26177bf761681bff121e6606c
MichaelBotha-Repos <mbotha88@gmail.com>
2022-02-03 08:05:22 +0200

    Update Faceted Data.py

--- MSc Programming/Secure Software Development/Faceted Data.py
+++ MSc Programming/Secure Software Development/Faceted Data.py
@@ -16,35 +16,48 @@ Answers:
     > Does not require multiple systems for different levels of data secrecy.
 
     Cons:
-    > May decrease code performance due to additional checking requried.
+    > May decrease code performance due to additional checking required.
     > Increases data size due to the additional metadata appended to each value.
 
 3.) Please refer to the below program, where a class is created to represent data
     in a system. The class inherits a standard Python data type and adds the features
     inherent in faceted data viz. owners and views. This meta data will be checked
     whenever a user wants to access or alter data.
-    
+
+    Another method is to simply create a list with the first element being the
+    original data and the second,third and so on the meta data.   
 
 """
-# Class creation
-class FacetedString(str):
+
+
+# Class creation.
+class FacetedList(list):
     """
         This class extends the native string class
         by adding faceted metadata
     """
     
-    def __init__(self, string):
+    def __init__(self, data, owner, view):
         """ An object is initialised by adding the faceted data
             elements to it"""
 
-        super().__init__()
-        #self.string = string + "<<>>" + user + view
+        list.__init__(self, data)
+        self.owner = owner
+        self.view = view
+        
 
-# Example use
+# Example use:
 
-test_data = "Bank: FNB ID: 85467834"
-faceted_test_data = FacetedString(test_data)
-#print(faceted_test_data.string)
+test_data = [1234, 987, 100, 5231]
+faceted_test_data = FacetedList(test_data, "Michael", 1000)
+
+# The faceted list behaves the same as a normal list
+# however has meta values.
+for elem in faceted_test_data:
+    print(elem)
+
+print(faceted_test_data.owner)
+print(faceted_test_data.view)
 
 
 
