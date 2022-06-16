8b893fd9595e572966fbbc6f1bf7c6f80bddc389
MichaelBotha-Repos <mbotha88@gmail.com>
2022-06-16 20:36:44 +1000

    update

--- Independent Projects and Studies/PCPP1/extend_Function_arguments.py
+++ Independent Projects and Studies/PCPP1/extend_Function_arguments.py
@@ -1,8 +1,7 @@
-# Normal function invocation:
 def without_arg():
     print('Hello')
 
-def with_arg(arg1, arg2=2, arg3=3):
+def with_arg(arg1=7, arg2=2, arg3=3):
     print(f'{arg1}:{arg2}:{arg3}')
 
 def new(*args, **kwargs):
@@ -10,7 +9,15 @@ def new(*args, **kwargs):
         print(elem)
     for key in kwargs:
         print(f"{key}->{kwargs[key]}")
+    new2(*args, **kwargs)
+
+def new2(*args,**kwargs):
+    print(args, kwargs)
+
 
 without_arg()
-with_arg(5, arg3=7)
+with_arg(arg3=7)
 new(6,7,8,9,blob=10,add='h')
+new(1,2,3,4, breakfast="cereal", lunch="chops", supper="salad")
+
+
