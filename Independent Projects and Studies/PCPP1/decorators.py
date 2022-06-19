046ff1bb019582b4f05fcdca2bcde61949226ee0
MichaelBotha-Repos <mbotha88@gmail.com>
2022-06-19 21:11:02 +1000

    update

--- Independent Projects and Studies/PCPP1/decorators.py
+++ Independent Projects and Studies/PCPP1/decorators.py
@@ -19,3 +19,42 @@ string_timestamp = timestamp.strftime('%Y-%m-%d %H:%M:%S')
 print(string_timestamp)
 
 """
+
+from datetime import datetime
+from random import randrange
+from time import sleep
+
+execution = []
+
+def timestamp(func):
+    global execution
+    def wrapper(*args):
+        timestamp = datetime.now()
+        string_timestamp = timestamp.strftime('%Y-%m-%d %H:%M:%S')
+        execution.append([string_timestamp, func.__name__])
+        return func(*args)
+    return wrapper 
+
+@timestamp
+def add(*args):
+        return sum(args) 
+@timestamp
+def sub(*args):
+    total = args[0]
+    for i in range(1, len(args)-1):
+        total -= args[i]
+    return total 
+
+if __name__ == "__main__":
+    #functions = [add, sub]
+    num = 0
+    while num != 3:
+        #print(functions[randrange(0, 2)](10, 7 , 6))
+        print(add(10, 7 , 6))
+        sleep(1)
+        print(sub(10, 7 , 6))
+        sleep(1)
+        num += 1
+
+
+print(execution)
