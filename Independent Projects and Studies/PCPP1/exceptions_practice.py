eee452f56099105ffc177fae939ec9f51db53ea2
MichaelBotha-Repos <mbotha88@gmail.com>
2022-06-21 20:35:40 +1000

    night work

---
+++ Independent Projects and Studies/PCPP1/exceptions_practice.py
@@ -0,0 +1,16 @@
+"""
+try:
+
+    user_input = int(input())
+    x = 10/user_input
+
+except:
+    print("failure")
+else:
+    print(x)
+finally:
+    print("finished")
+
+"""
+
+print(Exception.__subclasses__())
