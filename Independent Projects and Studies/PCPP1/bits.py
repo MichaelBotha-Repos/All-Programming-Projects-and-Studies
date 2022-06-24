70b1d59359cf8066ca998bd679d73434a0082e2f
MichaelBotha-Repos <mbotha88@gmail.com>
2022-06-25 09:54:21 +1000

    comments

--- Independent Projects and Studies/PCPP1/bits.py
+++ Independent Projects and Studies/PCPP1/bits.py
@@ -1,8 +1,15 @@
+
+# Bit representation:
+# D3-D2-D1-D0-A2-A1-A0-Data_Startbit (two start bits required)
 first_frame = 0b00010001
+# Bit representation:
+# D7-D6-D5-D4-A2-A1-A0-Data_Startbit (two start bits required)
 second_frame = 0b00000001
+# Combining the two frames to send in a single pyserial function call 
+# so as to ensure no time difference between the two
 data_stream = first_frame | (second_frame << 8)
 
-
-print(first_frame)
+print(first_frame, type(first_frame))
 print(second_frame)
+# Printing the data stream in binary showing 16bits 
 print(f"{data_stream : 16b}")
