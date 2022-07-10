e6e0fc3c4dbfa46993972db0310ce1763a05bad0
MichaelBotha-Repos <mbotha88@gmail.com>
2022-07-10 17:32:13 +1000

    C Updates

--- Independent Projects and Studies/PCPP1/tkinter_practice1.py
+++ Independent Projects and Studies/PCPP1/tkinter_practice1.py
@@ -12,7 +12,14 @@ def ecovar_test():
 main_window = tkinter.Tk() # Creation of main application window 
 main_window.title('EcoJoule Tester') 
 button1 = tkinter.Button(main_window, text = "EcoVar Tests", command = ecovar_test)
-button1.place( x = 10, y=10)
+button1.pack()
+
+switch = tkinter.IntVar()
+switch = 0
+checkbox = tkinter.Checkbutton(main_window, text = 'EvoVAR Test', variable = switch)
+checkbox.pack()
+if switch == 1:
+    print("ok")
 main_window.mainloop() # Launch event controller 
 
 
