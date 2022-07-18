149cdc6242003f7c32f3a7410c00eca3ff79cf3c
MichaelBotha-Repos <mbotha88@gmail.com>
2022-07-18 20:51:21 +1000

    Computer logic studies

--- Independent Projects and Studies/Beginner C Course/whiteboard_program.c
+++ Independent Projects and Studies/Beginner C Course/whiteboard_program.c
@@ -2,21 +2,9 @@
 
 void main()
 {
-    int counter = 0;
-    
-    do 
-    {
-        printf("Fun in the sun\n");
-        //counter --;
-    }while(counter > 0);
+   // or is a disjunction
+   // and is a conjunction
 
-    for(int i = 0; i < 10; i++)
-    {
-        if(i == 5)continue;
-        printf("%i -> in my for loop\n", i);
-
-        if(i == 8)break;
-
-    }
+   
 
 }
