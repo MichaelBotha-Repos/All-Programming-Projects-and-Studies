554348710c717ec6d65af9b1efa937c102822f86
MichaelBotha-Repos <mbotha88@gmail.com>
2023-05-24 23:00:31 +1000

    WIP

--- MSc Programming/Software Engineering Project Management/Estimation Program/estimation.py
+++ MSc Programming/Software Engineering Project Management/Estimation Program/estimation.py
@@ -1,11 +1,13 @@
-"""This progam will implement the Expert Judgment methodology of project estimation"""
+"""This progam will implement the Planning Poker methodology of project estimation"""
+
+import sqlite3
 
 class Task:
     """Represents a task"""
     def __init__(self):
         self.task_description = ''
         self.task_estimations = []
-        self.task_dependencies = []
+        # self.task_dependencies = []
         self.chosen_estimation = 0
         self.add_description()
         self.add_estimations()
@@ -28,7 +30,7 @@ class Task:
 class Project:
     """Represents a project with all allocated tasks and assigned efforts"""
     def __init__(self):
-        self.project_id = ""
+        self.project_id = input('Please insert a project name:\n')
         self.tasks = []
 
     def add_task(self):
@@ -36,17 +38,18 @@ class Project:
         self.tasks.append(Task())
 
     def display_tasks(self):
-        print("\nTask Description\tTask Estimation 1\tTask Estimation 2\tTask Estimation 3\tTask Estimation 4\tTask Estimation 5")
+        print("\nTask Description\tTask Estimation 1\tTask Estimation 2\tTask Estimation 3\tTask Estimation 4\tTask Estimation 5\tChosen Estimation")
         for task in self.tasks:
-            print(f"{task.task_description}\t\t\t{task.task_estimations[0]}\t\t\t{task.task_estimations[1]}\t\t\t{task.task_estimations[2]}\t\t\t{task.task_estimations[3]}\t\t\t{task.task_estimations[4]}")
+            print(f"{task.task_description}\t\t\t{task.task_estimations[0]}\t\t\t{task.task_estimations[1]}\t\t\t{task.task_estimations[2]}\t\t\t{task.task_estimations[3]}\t\t\t{task.task_estimations[4]}\t\t\t{task.chosen_estimation}")
             
         print('\n\n')
+        
        
         
 
-
 if __name__ == '__main__':
     # Main control flow of code
+    db_connection = sqlite3.connect('estimations.db')
     while True:
         print("Welcome")
         print("Please select and option:")
@@ -62,7 +65,8 @@ if __name__ == '__main__':
                 print("1 - Create a new task")
                 print("2 - Edit an existing task")
                 print("3 - Display all tasks")
-                print("4 - Exit")
+                print("4 - Save Project")
+                print("5 - Exit")
                 choice2 = input()
                 if choice2 == '1':
                     new_project.add_task()
@@ -71,7 +75,23 @@ if __name__ == '__main__':
                 elif choice2 == '3':
                     new_project.display_tasks()
                 elif choice2 == '4':
+                    db_connection_cursor = db_connection.cursor()
+                    command1 = f"CREATE TABLE {new_project.project_id}("
+                    command2 = '''TaskID INTEGER PRIMARY KEY AUTOINCREMENT,
+                                Task_description TEXT,
+                                Estimate1 INTEGER,
+                                Estimate2 INTEGER,
+                                Estimate3 INTEGER,
+                                Estimate4 INTEGER,
+                                Estimate5 INTEGER );'''
+                    db_connection_cursor.execute(command1+command2)
+                    for task in new_project.tasks:
+                        db_connection_cursor.execute(f"INSERT INTO {new_project.project_id} (Task_description,Estimate1, Estimate2, Estimate3, Estimate4, Estimate5) VALUES ('{task.task_description}' {', ' + ', '.join(task.task_estimations)})")
+                elif choice2 == '5':
                     break
+
+        elif choice == '2':
+            pass
 
         elif choice == '3':
             exit()
